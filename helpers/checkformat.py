f0253f3c896a05657f5601ba0ec5f2ce98104906
markessam12 <mark.essam.hakim@gmail.com>
2021-07-14 14:07:56 +0200

    handling more corner cases in checkformat

--- helpers/checkformat.py
+++ helpers/checkformat.py
@@ -2,22 +2,34 @@ def check_format(text):
     stored = []
     storing = False
     closing = False
+    closing_bracket_missing = False
     current_tag = ""
+    i = 0
     for i in range(len(text)):
         if text[i] == '<':
+            if closing_bracket_missing:
+                # Open tag missing closing bracket
+                return i
             if text[i + 1] == '/':
                 closing = True
+            elif text[i + 1] in ['?', '!']:
+                closing_bracket_missing = True
+                continue
+            closing_bracket_missing = True
             storing = True
 
         elif (text[i] == '>' or text[i] == ' ') and storing:
             if closing:
                 if current_tag[1:len(current_tag)] != stored.pop():
+                    # closing tag different from the open tag
                     return i - len(current_tag) + 1
                 elif text[i] == ' ':
+                    # closing tag contain space
                     return i - len(current_tag) + 1
             else:
-                if text[i-1] == '<':
-                    return i-1
+                if text[i - 1] == '<':
+                    # open tag starting with space or has no name {"<>" or "< tag>"}
+                    return i - 1
                 stored.append(current_tag)
             current_tag = ""
             storing = False
@@ -25,5 +37,21 @@ def check_format(text):
 
         elif storing:
             current_tag = current_tag + text[i]
-    return len(stored) == 0
+
+        if text[i] == '>':
+            if text[i-1] == '/':   # self closing tag
+                if text[i-2] == '<':
+                    # close tag has no name {"<\>"}
+                    return i-2
+                stored.pop()
+            if closing_bracket_missing:
+                closing_bracket_missing = False
+            else:
+                # extra closing bracket
+                return i
+    # remaining not closed tags
+    if len(stored) == 0:
+        return True
+    else:
+        return i
 
